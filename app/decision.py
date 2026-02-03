4ae2aa2dd88ce476b4ee84cf3f5b8ade96c2deab
kap644 <kapilsing3412@gmail.com>
2026-02-03 20:39:05 +0530

    Update decision.py

--- app/decision.py
+++ app/decision.py
@@ -1,9 +1,10 @@
 def classify(confidence):
-    if confidence >= 0.92:
+    if confidence >= 0.65:
         return "Human"
-    elif confidence <= 0.55:
+    elif confidence <= 0.40:
         return "AI"
     else:
         return "Suspicious"
+
 
 
